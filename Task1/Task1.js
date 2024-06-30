fab9c0454bcc28704e8a2adab398ab6e1e544a8b
OmkarFire <omkarskumbhar18@gmail.com>
2024-06-30 09:02:13 +0530

    Task1 completed

---
+++ Task1/Task1.js
@@ -0,0 +1,9 @@
+const navMenu = document.getElementById("nav-menu");
+
+navMenu.addEventListener("mouseenter", function () {
+  navMenu.style.backgroundColor = "#555";
+});
+
+navMenu.addEventListener("mouseleave", function () {
+  navMenu.style.backgroundColor = "#333";
+});
